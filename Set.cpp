dced384c9c86cbf08da54670a453452d33e13b51
Benjamin Lieser <benjamin.lieser@gmx.de>
2015-10-15 17:41:12 +0200

    First Implmentation of Set
    I think, that Set kan be a good class to derive from for subgroups, sightclasses, etc.

--- Set.cpp
+++ Set.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Set.h"
+
+
+
+template <int ORDER> Set<ORDER>::Set(std::vector<EW>* all) {
+    this->bitset.reset();
+    this->all = all;
+}
+
+template <int ORDER> Set<ORDER>::Set(const Set& origin) {
+    this->bitset = origin.bitset;
+    this->all = origin.all;
+}
+
+template <int ORDER> Set<ORDER> Set<ORDER>::merge(const Set& other) const {
+    Set<ORDER> set;
+    set.bitset = this->bitset |= other.bitset;
+    set.all = this->all;
+    return set;
+}
+
+template <int ORDER> Set<ORDER> Set<ORDER>::intersection(const Set& other) const {
+    Set<ORDER> set;
+    set.bitset = this->bitset &= other.bitset;
+    set.all = this->all;
+    return set;
+}
--- Set.h
+++ Set.h
@@ -7,8 +7,25 @@
 
 //This class should represent a set of Elements. The operations of add an element, check whether an element is in the set , merging, and intersection, should be implemented
 
-class Set {
+#include <bitset>
+#include "ElementWrapper.h"
 
+template <int ORDER>
+class Set {
+public:
+    Set(std::vector<EW>* all);
+    Set(std::vector<EW*> set, std::vector<EW>* all);
+    Set(const Set& orign);
+    virtual ~Set();
+
+    Set merge(const Set& other) const;
+    Set intersection(const Set& other) const;
+    std::vector<EW*> get(const std::vector<EW>& all) const;
+    int order() const; //Number of element in the set
+
+private:
+    std::bitset<ORDER> bitset; //Which element is in the set, 0 for is not in the set, 1 for it is
+    std::vector<EW>* all; //Pointer to the original Elements
 };
 
 
