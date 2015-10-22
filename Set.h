5006f9b762fb6ecc7ee5fdb7cf698b934876a473
ColinRothgang <colin.rothgang@gmx.de>
2015-10-22 18:06:32 +0200

    implemented alternative implementation of Sets

--- Set.h
+++ Set.h
@@ -4,28 +4,25 @@
 
 #ifndef FGA_SET_H
 #define FGA_SET_H
+#include <vector>
 
 //This class should represent a set of Elements. The operations of add an element, check whether an element is in the set , merging, and intersection, should be implemented
 
-#include <bitset>
-#include "ElementWrapper.h"
-
-template <int ORDER>
+template <class T>
 class Set {
 public:
-    Set(std::vector<EW>* all);
-    Set(std::vector<EW*> set, std::vector<EW>* all);
-    Set(const Set& orign);
+    Set(std::vector<T*> elements);
+    Set(const Set& origin);
     virtual ~Set();
 
-    Set merge(const Set& other) const;
-    Set intersection(const Set& other) const;
-    std::vector<EW*> get(const std::vector<EW>& all) const;
+    bool isElement(const T& element);
+    bool isSubset(const Set& S);
+    Set merge(const Set& other);
+    Set intersection(const Set& other);
+    std::vector<T*> get() const;
     int order() const; //Number of element in the set
-
 private:
-    std::bitset<ORDER> bitset; //Which element is in the set, 0 for is not in the set, 1 for it is
-    std::vector<EW>* all; //Pointer to the original Elements
+    std::vector<const T*> elements; //Pointer to the original Elements
 };
 
 
